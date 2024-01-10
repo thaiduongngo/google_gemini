bdd343088e73bc7ff650594dd272182439ed1126
thaiduong <thaiduong.ngo@gmail.com>
2024-01-10 22:26:16 +0700

    Added ocr to visions.

--- generative_ai/visions/__init__.py
+++ generative_ai/visions/__init__.py
@@ -1,13 +1,22 @@
 import google.generativeai as genai
-from google.generativeai import GenerativeModel
+from google.generativeai import GenerationConfig
 from generative_ai.configs import SAFETY_SETTINGS
+import os
 
 
-VISION_MODEL = "gemini-pro-visions"
+VISION_MODEL = "gemini-pro-vision"
 
 
-def init_vision_model() -> GenerativeModel:
+def init_vision_model():
+    genai.configure(api_key=os.environ.get("GOOGLE_API_KEY"))
+    generation_config = GenerationConfig(
+        temperature=0,
+        top_p=1,
+        top_k=32,
+        max_output_tokens=4096
+    )
     return genai.GenerativeModel(
         model_name=VISION_MODEL,
+        generation_config=generation_config,
         safety_settings=SAFETY_SETTINGS,
     )
---
+++ generative_ai/visions/vision.py
@@ -0,0 +1,33 @@
+from pathlib import Path
+from generative_ai.visions import init_vision_model
+
+
+def process_ocr(image_object: str | bytes, mime_type: str) -> str:
+    model = init_vision_model()
+    images = [
+        {
+            "mime_type": "image/jpeg",
+            "data": image_object
+        },
+    ]
+    prompt = [
+        "Extract complete text in the image(s) below:\n"
+    ]
+    for image in images:
+        prompt.append(image)
+    prompt.append("\nResponse: ")
+    response = model.generate_content(prompt)
+    return response.text
+
+
+def ocr_file(file_name: str) -> str:
+    if not (img := Path(file_name)).exists():
+        raise FileNotFoundError(f"Could not find image: {img}")
+    return process_ocr(image_object=img.read_bytes(), mime_type="image/jpeg")
+
+
+def ocr(b64_string: str) -> str:
+    splits = b64_string.split(";base64,")
+    b64_str = splits[1]
+    mine_type = splits[0].replace("data:", "")
+    return process_ocr(image_object=b64_str, mime_type=mine_type)
